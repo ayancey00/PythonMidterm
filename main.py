e97f1186575151899ec4c98c6383ae6c35b5709f
ayancey00 <adam.yancey@okstate.edu>
2026-02-16 11:03:49 -0600

    Updated with board and Print board

--- main.py
+++ main.py
@@ -14,13 +14,22 @@
 import random
 
 #setup for the grid
-rows, cols = 50, 50
+rows, cols = 20, 20
 grid = [[0 for _ in range(cols)] for _ in range(rows)]
 
 # This is so 25% of cells start alive
-p_alive = 0.25  
+p_alive = 0.25 
 
 #this sets up the board. Giving 25 percent of them life and leaving 75 percent dead.
-for rows in range(rows):
-    for cols in range(cols):
-        grid[rows][cols] = 1 if random.random() < p_alive else 0
+for r in range(rows):
+    for c in range(cols):
+        grid[r][c] = 1 if random.random() < p_alive else 0
+
+
+# This prints our grid with 1's and 0's
+for row in grid:
+        line = "".join("1" if cell else "." for cell in row)
+        print(line)
+
+
+# now we need the neighbors to be counted
