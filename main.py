0285ad6f5a7acf0baebef112185896d8c4ab5690
ayancey00 <adam.yancey@okstate.edu>
2026-02-16 09:51:09 -0600

    Updated Things we need!

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+# Need a 2d grid, Grid[c,r] possiblely.  
 
+# Each block we refer to as a cell. cell can either be one of zero. Alive or dead
+
+# There will be 8 cells around any given cell(Neighbors). If cell is alive and if 2-3 neigbors are alive, it stays alive. anything else, dead.
+# When cell is dead and there is exactly three alive neighbors, then it becomes alive.
+# we need to make it wrap around, when it reaches the left side it wraps over to the right side.
